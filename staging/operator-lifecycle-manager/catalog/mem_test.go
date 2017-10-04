c13fab93dede866b0661ac4c38369fa1f8122bb4
EvB <erica.vonbuelow@coreos.com>
2017-10-04 11:41:23 -0400

    refactor(catalog/mem): remove sources subpackage, enforce interface
    Upstream-repository: operator-lifecycle-manager
    Upstream-commit: c530e4d1a126fba26d90f2051608b4d7b829c855

--- staging/operator-lifecycle-manager/catalog/mem_test.go
+++ staging/operator-lifecycle-manager/catalog/mem_test.go
@@ -3,15 +3,14 @@ package catalog
 import (
 	"testing"
 
+	"github.com/coreos-inc/alm/apis/clusterserviceversion/v1alpha1"
 	"github.com/stretchr/testify/assert"
 	"k8s.io/apiextensions-apiserver/pkg/apis/apiextensions"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
-	"github.com/coreos-inc/alm/apis/clusterserviceversion/v1alpha1"
 )
 
-func TestFindClusterServiceVersionByServiceName(t *testing.T) {
-	catalog := NewInMem()
+func TestFindClusterServiceVersionByName(t *testing.T) {
+	catalog := NewMemoryMap()
 	csv1 := v1alpha1.ClusterServiceVersion{
 		TypeMeta: metav1.TypeMeta{
 			Kind:       v1alpha1.ClusterServiceVersionCRDName,
@@ -28,7 +27,7 @@ func TestFindClusterServiceVersionByServiceName(t *testing.T) {
 	crds := []apiextensions.CustomResourceDefinition{crd1}
 	catalog.addService(csv1, crds)
 
-	foundCSV, err := catalog.FindClusterServiceVersionByServiceName("MockCSV")
+	foundCSV, err := catalog.FindClusterServiceVersionByName("MockCSV")
 	assert.NoError(t, err)
 	assert.Equal(t, "MockCSV", foundCSV.GetName())
 }
