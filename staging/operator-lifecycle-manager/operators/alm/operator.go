a9441b778abce06a765995709cfc649e5b7a52e9
Evan Cordell <cordell.evan@gmail.com>
2017-10-30 15:33:48 -0400

    Refactor deployment strategy client + naming fixes
    (upstream operator-lifecycle-manager commit: 65a0c84203a6f7fed85bf3756f52eb58f5116409)

--- staging/operator-lifecycle-manager/operators/alm/operator.go
+++ staging/operator-lifecycle-manager/operators/alm/operator.go
@@ -134,7 +134,7 @@ func (a *ALMOperator) transitionCSVState(csv *v1alpha1.ClusterServiceVersion) (s
 			csv.SetPhase(v1alpha1.CSVPhaseFailed, v1alpha1.CSVReasonInvalidStrategy, fmt.Sprintf("install strategy invalid: %s", err))
 			return
 		}
-		installer := a.resolver.InstallerForStrategy(strategy.GetStrategyName(), a.OpClient, csv.ObjectMeta, csv.TypeMeta)
+		installer := a.resolver.InstallerForStrategy(strategy.GetStrategyName(), a.OpClient, csv.ObjectMeta)
 		installed, err := installer.CheckInstalled(strategy)
 		if err != nil {
 			// TODO: add a retry count, don't give up on first failure
@@ -167,7 +167,7 @@ func (a *ALMOperator) transitionCSVState(csv *v1alpha1.ClusterServiceVersion) (s
 			csv.SetPhase(v1alpha1.CSVPhaseFailed, v1alpha1.CSVReasonInvalidStrategy, fmt.Sprintf("install strategy invalid: %s", err))
 			return
 		}
-		installer := a.resolver.InstallerForStrategy(strategy.GetStrategyName(), a.OpClient, csv.ObjectMeta, csv.TypeMeta)
+		installer := a.resolver.InstallerForStrategy(strategy.GetStrategyName(), a.OpClient, csv.ObjectMeta)
 		installed, err := installer.CheckInstalled(strategy)
 
 		// if already installed, don't transition to pending if we can't query
