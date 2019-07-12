cd6fb9a514be33f00b8613bf2f12996cde235224
Evan Cordell <cordell.evan@gmail.com>
2019-07-12 09:27:50 -0400

    feat(metrics): record sync count for subscriptions, labeled with name and most recently installed CSV version
    (upstream operator-lifecycle-manager commit: 8e9c5235918c19877a181e722763ff2e4fd735eb)

--- staging/operator-lifecycle-manager/pkg/controller/operators/olm/operator.go
+++ staging/operator-lifecycle-manager/pkg/controller/operators/olm/operator.go
@@ -708,8 +708,6 @@ func (a *Operator) syncClusterServiceVersion(obj interface{}) (syncError error) 
 		return
 	}
 
-	a.recordMetrics(logger, clusterServiceVersion)
-
 	outCSV, syncError := a.transitionCSVState(*clusterServiceVersion)
 
 	if outCSV == nil {
@@ -807,18 +805,6 @@ func (a *Operator) syncGcCsv(obj interface{}) (syncError error) {
 		return
 	}
 	return
-}
-
-func (a *Operator) recordMetrics(logger *logrus.Entry, csv *v1alpha1.ClusterServiceVersion) {
-	if csv.IsCopied() {
-		// don't record for copied CSVs
-		return
-	}
-	counter, err := metrics.CounterForCSV(csv.GetName(), string(csv.Status.Phase))
-	if err != nil {
-		logger.WithError(err).Warn("could not record metrics")
-	}
-	counter.Inc()
 }
 
 // operatorGroupFromAnnotations returns the OperatorGroup for the CSV only if the CSV is active one in the group
