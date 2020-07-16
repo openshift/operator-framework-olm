a91786a6f258826c96403daf18b23df0732614c4
Evan <cordell.evan@gmail.com>
2020-07-16 11:47:31 -0400

    renaming / refactoring
    this should make it simple to remove the old resolver when the time
    is right
    
    (upstream operator-lifecycle-manager commit: 6538c8dd881dfe91306329a91cd079c857a5c4d7)

--- staging/operator-lifecycle-manager/pkg/controller/operators/catalog/operator.go
+++ staging/operator-lifecycle-manager/pkg/controller/operators/catalog/operator.go
@@ -132,11 +132,11 @@ func NewOperator(ctx context.Context, kubeconfigPath string, clock utilclock.Clo
 	// Create an OperatorLister
 	lister := operatorlister.NewLister()
 
-	var res resolver.Resolver
+	var res resolver.StepResolver
 	if resolverV2Enable {
-		res = resolver.NewSatStepResolver(lister, crClient, opClient.KubernetesInterface(), operatorNamespace, logger)
+		res = resolver.NewOperatorStepResolver(lister, crClient, opClient.KubernetesInterface(), operatorNamespace, logger)
 	} else {
-		res = resolver.NewOperatorsV1alpha1Resolver(lister, crClient, opClient.KubernetesInterface(), operatorNamespace)
+		res = resolver.NewLegacyResolver(lister, crClient, opClient.KubernetesInterface(), operatorNamespace)
 	}
 	successMetricsEmitter := metrics.RegisterDependencyResolutionSuccess
 	failureMetricsEmitter := metrics.RegisterDependencyResolutionFailure
