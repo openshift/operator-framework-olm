04ffb8e7f67e4f77da9bc4129c085237050be1c5
timflannagan <timflannagan@gmail.com>
2021-03-16 16:22:50 -0400

    Introduce cmds package that tracks the staging operator-framework directories

---
+++ cmds.go
@@ -0,0 +1,7 @@
+package cmds
+
+import (
+	_ "github.com/operator-framework/operator-lifecycle-manager/cmd/catalog"
+	_ "github.com/operator-framework/operator-lifecycle-manager/cmd/olm"
+	_ "github.com/operator-framework/operator-registry/cmd/opm"
+)
