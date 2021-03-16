bd0a2a57ceffb64424744d8607ac58cbb0b242df
timflannagan <timflannagan@gmail.com>
2021-03-16 18:35:59 -0400

    Add the package-server to the list of local command packages

--- cmds.go
+++ cmds.go
@@ -3,5 +3,6 @@ package cmds
 import (
 	_ "github.com/operator-framework/operator-lifecycle-manager/cmd/catalog"
 	_ "github.com/operator-framework/operator-lifecycle-manager/cmd/olm"
+	_ "github.com/operator-framework/operator-lifecycle-manager/cmd/package-server"
 	_ "github.com/operator-framework/operator-registry/cmd/opm"
 )
