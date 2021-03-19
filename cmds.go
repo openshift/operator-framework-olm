8bb5d2baf394dac4ab22d27be79fef09ab3bdc13
timflannagan <timflannagan@gmail.com>
2021-03-19 15:00:28 -0400

    cmds.go,vendor: Introduce the non-opm registry binaries to the cmds package
    Update the list of local command binaries and add the non-opm (e.g.
    deprecated) registry command packages so we can build and test them
    using the root vendor tree.

--- cmds.go
+++ cmds.go
@@ -4,5 +4,10 @@ import (
 	_ "github.com/operator-framework/operator-lifecycle-manager/cmd/catalog"
 	_ "github.com/operator-framework/operator-lifecycle-manager/cmd/olm"
 	_ "github.com/operator-framework/operator-lifecycle-manager/cmd/package-server"
+
+	_ "github.com/operator-framework/operator-registry/cmd/appregistry-server"
+	_ "github.com/operator-framework/operator-registry/cmd/configmap-server"
+	_ "github.com/operator-framework/operator-registry/cmd/initializer"
 	_ "github.com/operator-framework/operator-registry/cmd/opm"
+	_ "github.com/operator-framework/operator-registry/cmd/registry-server"
 )
