a5c25b3de148d9bf51ec37714dcbb21642ded907
timflannagan <timflannagan@gmail.com>
2021-03-20 18:03:41 -0400

    scripts: Check if the vendor directory needs to be populated first before running e2e packages

--- scripts/e2e.sh
+++ scripts/e2e.sh
@@ -16,8 +16,15 @@ ROOT_DIR=$(dirname "${BASH_SOURCE[0]}")/..
 
 function run_test() {
     local staging_dir=$1
+    local path_to_staging_dir="${ROOT_DIR}/staging/${staging_dir}"
 
-    pushd "${ROOT_DIR}/staging/${staging_dir}"
+    pushd "${path_to_staging_dir}"
+
+    if [[ ! -d ${path_to_staging_dir}/vendor ]]; then
+        echo "Populating nested vendor directory"
+        go mod tidy && go mod vendor && go mod verify
+    fi
+
     echo "Running ${staging_dir} e2e tests"
     make e2e
     popd
