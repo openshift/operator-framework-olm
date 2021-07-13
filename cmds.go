8c2e98ae1ed1947ad509a41033fa66cbce18ab9b
Ben Luddy <bluddy@redhat.com>
2021-07-13 09:43:32 -0400

    Build cpb directly from staging.

--- cmds.go
+++ cmds.go
@@ -4,6 +4,7 @@ import (
 	_ "github.com/operator-framework/operator-lifecycle-manager/cmd/catalog"
 	_ "github.com/operator-framework/operator-lifecycle-manager/cmd/olm"
 	_ "github.com/operator-framework/operator-lifecycle-manager/cmd/package-server"
+	_ "github.com/operator-framework/operator-lifecycle-manager/util/cpb"
 
 	_ "github.com/operator-framework/operator-registry/cmd/configmap-server"
 	_ "github.com/operator-framework/operator-registry/cmd/initializer"
