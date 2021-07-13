069b3ed5c9d118bc478d310ab092f8041a41cfcf
Ben Luddy <bluddy@redhat.com>
2021-07-13 09:46:38 -0400

    Add grpc-health-probe to cmds (part of registry image build).

--- cmds.go
+++ cmds.go
@@ -6,6 +6,7 @@ import (
 	_ "github.com/operator-framework/operator-lifecycle-manager/cmd/package-server"
 	_ "github.com/operator-framework/operator-lifecycle-manager/util/cpb"
 
+	_ "github.com/grpc-ecosystem/grpc-health-probe"
 	_ "github.com/operator-framework/operator-registry/cmd/configmap-server"
 	_ "github.com/operator-framework/operator-registry/cmd/initializer"
 	_ "github.com/operator-framework/operator-registry/cmd/opm"
