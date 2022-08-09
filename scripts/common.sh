3ef00f08ab3327eeeb90aaf451bf209c4bf541b6
timflannagan <timflannagan@gmail.com>
2022-08-09 12:55:19 -0400

    scripts: Add the pkg/manifests as a known generated file path

--- scripts/common.sh
+++ scripts/common.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-export KNOWN_GENERATED_PATHS=(':!vendor' ':!manifests' ':!go.sum' ':!go.mod')
+export KNOWN_GENERATED_PATHS=(':!vendor' ':!pkg/manifests' ':!manifests' ':!go.sum' ':!go.mod')
 export UPSTREAM_REMOTES=("api" "operator-registry" "operator-lifecycle-manager")
