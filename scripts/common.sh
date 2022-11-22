2f7ea94df81b457355cbd19b28898aa88bcd6e77
Joe Lanford <joe.lanford@gmail.com>
2022-11-22 17:46:44 -0500

    semver veneer: error when unknown fields are encountered in input file (#1043)
    Signed-off-by: Joe Lanford <joe.lanford@gmail.com>
    
    Signed-off-by: Joe Lanford <joe.lanford@gmail.com>
    Upstream-repository: operator-registry
    Upstream-commit: 0080ea0a938fdb91662a6ca36387aa71d3a52b28

--- scripts/common.sh
+++ scripts/common.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-export KNOWN_GENERATED_PATHS=(':!vendor' ':!pkg/manifests' ':!manifests' ':!go.sum' ':!go.mod')
+export KNOWN_GENERATED_PATHS=(':!vendor' ':!pkg/manifests' ':!manifests' ':!go.sum' ':!go.mod' ':!.gitignore' ':!operator-registry.blacklist' ':!api.blacklist' ':!operator-lifecycle-manager.blacklist' ':!scripts/common.sh')
 # TODO(tflannag): This is hacky but works in the current setup.
 export ROOT_GENERATED_PATHS=( "vendor" "pkg/manifests" "manifests" "go.mod" "go.sum" )
 export UPSTREAM_REMOTES=("api" "operator-registry" "operator-lifecycle-manager")
