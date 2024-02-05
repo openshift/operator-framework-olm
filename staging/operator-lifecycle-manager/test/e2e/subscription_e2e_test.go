79fe5018539c0ec558bd8c4f8dacdcc71f513e4d
Anik Bhattacharjee <anbhatta@redhat.com>
2024-02-05 14:20:27 -0500

    Reflect BundleUnpacking cond removal in missed e2e test (#3170)
    PR #3166 removes the `BundleUnpacking` condition once resolution is successful.
    PR #3166 [modified an e2e test](https://github.com/operator-framework/operator-lifecycle-manager/commit/54da66a9996632315827ba37e14823de6405b4d9#diff-11f70fc71ac22d725767916f562789de88d06eb9ebe19f337a59fd7035a3ca2dR2448) to reflect that change.
    However, the test being fixed in this PR is skipped for upstream, and runs only
    downstream.This PR is a follow up to #3166 to reflect the `BundleUnpacking` condition
    removal in the remaining test.
    
    Signed-off-by: Anik Bhattacharjee <anbhatta@redhat.com>
    Upstream-repository: operator-lifecycle-manager
    Upstream-commit: 6e3f051a4d9ac32222a051029570b88c8ba9e4f6

--- staging/operator-lifecycle-manager/test/e2e/subscription_e2e_test.go
+++ staging/operator-lifecycle-manager/test/e2e/subscription_e2e_test.go
@@ -2672,7 +2672,7 @@ properties:
 				if err != nil {
 					return err
 				}
-				if cond := fetched.Status.GetCondition(v1alpha1.SubscriptionBundleUnpacking); cond.Status != corev1.ConditionFalse {
+				if cond := fetched.Status.GetCondition(v1alpha1.SubscriptionBundleUnpacking); cond.Status != corev1.ConditionUnknown {
 					return fmt.Errorf("subscription condition %s has unexpected value %s, expected %s", v1alpha1.SubscriptionBundleUnpacking, cond.Status, corev1.ConditionFalse)
 				}
 				if cond := fetched.Status.GetCondition(v1alpha1.SubscriptionBundleUnpackFailed); cond.Status != corev1.ConditionUnknown {
