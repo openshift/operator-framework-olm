a6cd494df95b6fcca1598248e34915e8ab797ba1
kevinrizza <krizza@redhat.com>
2024-04-25 08:43:51 -0400

    UPSTREAM: <carry>: bump go-jose
    go-jose has a patched vulnerability -- https://nvd.nist.gov/vuln/detail/CVE-2024-28180
    operator-registry has an indirect dependency on that package as part of
    its grpc health probe. however, the current version of the grpc dependency relies
    on a deprecated version of the package. this carry commit updates
    github.com/grpc-ecosystem/grpc-health-probe to 0.4.24 so that we can update
    the indirect dependency to use the non deprecated repository for go-jose, then
    updates go-jose to v3.0.3 where the vulnerability is patched.
    
    Resolves https://issues.redhat.com/browse/OCPBUGS-32860
    
    Signed-off-by: kevinrizza <krizza@redhat.com>

--- vendor/github.com/spiffe/go-spiffe/v2/bundle/x509bundle/bundle.go
+++ vendor/github.com/spiffe/go-spiffe/v2/bundle/x509bundle/bundle.go
@@ -76,6 +76,23 @@ func Parse(trustDomain spiffeid.TrustDomain, b []byte) (*Bundle, error) {
 	return bundle, nil
 }
 
+// ParseRaw parses a bundle from bytes. The certificate must be ASN.1 DER (concatenated
+// with no intermediate padding if there are more than one certificate)
+func ParseRaw(trustDomain spiffeid.TrustDomain, b []byte) (*Bundle, error) {
+	bundle := New(trustDomain)
+	certs, err := x509.ParseCertificates(b)
+	if err != nil {
+		return nil, x509bundleErr.New("cannot parse certificate: %v", err)
+	}
+	if len(certs) == 0 {
+		return nil, x509bundleErr.New("no certificates found")
+	}
+	for _, cert := range certs {
+		bundle.AddX509Authority(cert)
+	}
+	return bundle, nil
+}
+
 // TrustDomain returns the trust domain that the bundle belongs to.
 func (b *Bundle) TrustDomain() spiffeid.TrustDomain {
 	return b.trustDomain
@@ -110,7 +127,7 @@ func (b *Bundle) RemoveX509Authority(x509Authority *x509.Certificate) {
 
 	for i, r := range b.x509Authorities {
 		if r.Equal(x509Authority) {
-			//remove element from slice
+			// remove element from slice
 			b.x509Authorities = append(b.x509Authorities[:i], b.x509Authorities[i+1:]...)
 			return
 		}
