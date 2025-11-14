bd367d7877c67f07986ccafcc9e29548df57284c
Jian Zhang <jiazha@redhat.com>
2025-11-14 18:35:44 +0800

    UPSTREAM: <carry>: migrate jiazha's OLMv0 test cases to OTE

--- tests-extension/test/qe/specs/olmv0_hypershiftmgmt.go
+++ tests-extension/test/qe/specs/olmv0_hypershiftmgmt.go
@@ -48,7 +48,7 @@ var _ = g.Describe("[sig-operator][Jira:OLM] OLMv0 on hypershift mgmt", g.Label(
 		oc.SetGuestKubeconf(guestClusterKube)
 	})
 
-	g.It("PolarionID:45381-[OTP][Skipped:Disconnected]Support custom catalogs in hypershift", g.Label("original-name:[sig-operator][Jira:OLM] OLMv0 on hypershift mgmt PolarionID:45381-[Skipped:Disconnected]Support custom catalogs in hypershift"), func() {
+	g.It("PolarionID:45381-[OTP][Skipped:Disconnected]Support custom catalogs in hypershift", func() {
 		var (
 			itName              = g.CurrentSpecReport().FullText()
 			buildPruningBaseDir = exutil.FixturePath("testdata", "olm")
@@ -117,7 +117,7 @@ var _ = g.Describe("[sig-operator][Jira:OLM] OLMv0 on hypershift mgmt", g.Label(
 
 	})
 
-	g.It("PolarionID:45408-[OTP][Skipped:Disconnected]Eliminate use of imagestreams in catalog management", g.Label("original-name:[sig-operator][Jira:OLM] OLMv0 on hypershift mgmt PolarionID:45408-[Skipped:Disconnected]Eliminate use of imagestreams in catalog management"), func() {
+	g.It("PolarionID:45408-[OTP][Skipped:Disconnected]Eliminate use of imagestreams in catalog management", func() {
 		controlProject := hostedClusterNS + "-" + guestClusterName
 		if !isAKS {
 			exutil.SkipBaselineCaps(oc, "None")
@@ -152,5 +152,62 @@ var _ = g.Describe("[sig-operator][Jira:OLM] OLMv0 on hypershift mgmt", g.Label(
 			}
 		}
 	})
+	// Polarion ID: 45543
+	g.It("PolarionID:45543-[OTP][Skipped:Disconnected]Enable hypershift to deploy OperatorLifecycleManager resources", func() {
 
+		g.By("1, check if any resource running in the guest cluster")
+		projects := []string{"openshift-operator-lifecycle-manager", "openshift-marketplace"}
+		for _, project := range projects {
+			resource, err := oc.AsGuestKubeconf().Run("get").Args("pods", "-n", project).Output()
+			if err != nil {
+				e2e.Failf("Fail to get resource in project: %s, error:%v", project, err)
+			}
+			// now, for guest cluster, there is may have a custom catalog resource for testing
+			if project == "openshift-marketplace" && strings.Contains(resource, "marketplace-operator") {
+				e2e.Failf("Found Marketplace related resources running on the guest cluster")
+			}
+			if project != "openshift-marketplace" && !strings.Contains(resource, "No resources found") {
+				e2e.Failf("Found OLM related resources running on the guest cluster")
+			}
+		}
+
+		buildPruningBaseDir := exutil.FixturePath("testdata", "olm")
+		dr := make(olmv0util.DescriberResrouce)
+		itName := g.CurrentSpecReport().FullText()
+		dr.AddIr(itName)
+
+		g.By("2, create an OperatorGroup")
+		ns := "guest-cluster-45543"
+		err := oc.AsGuestKubeconf().Run("create").Args("ns", ns).Execute()
+		o.Expect(err).NotTo(o.HaveOccurred())
+		defer oc.AsGuestKubeconf().Run("delete").Args("ns", ns).Execute()
+		ogSingleTemplate := filepath.Join(buildPruningBaseDir, "operatorgroup.yaml")
+		og := olmv0util.OperatorGroupDescription{
+			Name:      "og-45348",
+			Namespace: ns,
+			Template:  ogSingleTemplate,
+		}
+		defer og.Delete(itName, dr)
+		og.CreateWithCheck(oc.AsGuestKubeconf(), itName, dr)
+
+		g.By("3, subscribe to learn-operator.v0.0.3")
+		subTemplate := filepath.Join(buildPruningBaseDir, "olm-subscription.yaml")
+		sub := olmv0util.SubscriptionDescription{
+			SubName:   "sub-45348",
+			Namespace: ns,
+			// CatalogSourceName:      "cs-45348",
+			CatalogSourceName:      "qe-app-registry",
+			CatalogSourceNamespace: "openshift-marketplace",
+			Channel:                "beta",
+			IpApproval:             "Automatic",
+			OperatorPackage:        "learn",
+			StartingCSV:            "learn-operator.v0.0.3",
+			SingleNamespace:        true,
+			Template:               subTemplate,
+		}
+		defer sub.Delete(itName, dr)
+		sub.Create(oc.AsGuestKubeconf(), itName, dr)
+		defer sub.DeleteCSV(itName, dr)
+		olmv0util.NewCheck("expect", exutil.AsAdmin, exutil.WithoutNamespace, exutil.Compare, "Succeeded", exutil.Ok, []string{"csv", "learn-operator.v0.0.3", "-n", ns, "-o=jsonpath={.status.phase}"}).Check(oc.AsGuestKubeconf())
+	})
 })
